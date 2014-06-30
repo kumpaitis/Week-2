c77204bfc8f1ec73d92e5f8b2fdea50e7b53445d
Saulius Tumėnas <tumenas.s@gmail.com>
2014-06-30 19:29:55 +0300

    Added last task

---
+++ location_in_hierarchy.rb
@@ -0,0 +1,13 @@
+def location_in_hierarchy(object, method)
+  object_class = object.class
+  ancestors = [object_class]
+  while !(object_class = object_class.superclass).nil?
+      ancestors.push(object_class)
+  end
+    
+  ancestors.reverse.each do |ancestor|
+    if ancestor.instance_methods.include?(method)
+      return ancestor
+    end
+  end
+end 
